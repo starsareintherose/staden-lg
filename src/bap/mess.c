dfb25a03d8322032c483c6582ec8874ae88bb4ed
Kuoi <starsareintherose@outlook.com>
2023-04-12 20:47:23 +0800

    fix: compile broken

--- src/bap/mess.c
+++ src/bap/mess.c
@@ -1,8 +1,8 @@
-/* 
+/*
   Program Name: mess
   File name: mess.c
   Purpose: put user information to the screen for the text version
-         of osp--maybe error messages or any kind of user information
+	 of osp--maybe error messages or any kind of user information
 	 or question.
 
   Last Update: Tuesday 13 August 1991
@@ -15,47 +15,50 @@
 */
 
 /* --- includes --- */
-#include "defn.h" /* IMPORT: stdio.h */
-#include "textOutput.h" /* IMPORT: UpdateTextOutput */
-#include <stdio.h>
 #include <stdarg.h>
-/* int       vfprintf (FILE *stream, const char *format, va_list arglist); */
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "defn.h"	/* IMPORT: stdio.h */
+#include "textOutput.h" /* IMPORT: UpdateTextOutput */
+/* int       vfprintf (FILE *stream, const char *format, va_list arglist);
+ */
 
 /* ---- Exports ---- */
 void messagef(char *format, ...)
 {
-    va_list args;
-    va_start (args,format);
-    vfprintf (stdout, format, args);
-    va_end(args);
-    UpdateTextOutput();
+	va_list args;
+	va_start(args, format);
+	vfprintf(stdout, format, args);
+	va_end(args);
+	UpdateTextOutput();
 }
 
 void message(char *message)
 {
-  /* prints the message to the screen*/
-  fprintf(stdout,"%s",message);
-  UpdateTextOutput();
-  return;
+	/* prints the message to the screen*/
+	fprintf(stdout, "%s", message);
+	UpdateTextOutput();
+	return;
 }
 
 void popupMessage(char *message)
 {
-  fprintf(stdout,"%s",message);
-  UpdateTextOutput();
-  return;
+	fprintf(stdout, "%s", message);
+	UpdateTextOutput();
+	return;
 }
-
 
 void popUpErrorMessage()
 {
-  fprintf(stdout,"ERROR: Memory allocation problem.\nPlease exit and restart this program\n");
-/* since this is the text version I want the program to exit if this
-   memory allocation error occurs*/
-  UpdateTextOutput();
-  sleep(10);
+	fprintf(stdout,
+		"ERROR: Memory allocation problem.\nPlease exit and restart "
+		"this program\n");
+	/* since this is the text version I want the program to exit if this
+	   memory allocation error occurs*/
+	UpdateTextOutput();
+	sleep(10);
 
-  exit();
-
+	exit(0);
 }
 
